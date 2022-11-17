2f2fc55d0226230752171b235365837aa78df15d
Smillak <85848470+Smillak@users.noreply.github.com>
2022-11-17 19:39:09 +0100

    change

---
+++ Moveable.java
@@ -0,0 +1,5 @@
+interface MovableInterface{
+    void move();
+    void turnLeft();
+    void turnRight();
+}
---
+++ main.java
@@ -0,0 +1,9 @@
+import java.awt.Color;
+
+public class main{
+    public static void main(String[] args) {
+        Saab95 vehicle1 = new Saab95(2, 125, 2, Color.RED, "Saab95");
+        Volvo240 vehicle2 = new Volvo240();
+    }
+}
+
