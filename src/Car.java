1d691955234fd9831fadcd78c9a0c829a067ed91
Kimshii <117647269+Kimshii@users.noreply.github.com>
2022-11-28 10:54:12 +0100

    Update Car.java

--- src/Car.java
+++ src/Car.java
@@ -93,10 +93,6 @@ public abstract class Car implements Movable {
     public boolean getTurnRight() {
         return this.turnRight;
     }
-    
-    public boolean getTurnRight() {
-        return this.turnRight;
-    }
 
     public void move() {
         if (this.turnLeft == true) {
