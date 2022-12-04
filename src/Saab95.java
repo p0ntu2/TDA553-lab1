438da42d435c04334282e432c371418a1d88d767
Kimshii <117647269+Kimshii@users.noreply.github.com>
2022-12-04 17:46:43 +0100

    Update Saab95.java

--- src/Saab95.java
+++ src/Saab95.java
@@ -2,25 +2,27 @@ package src;
 import java.awt.*;
 
 public class Saab95 extends Car {
-    private boolean turboOn;
 
-    public Saab95(int doors, int power, int speed, Color color, String name, int x, int y, boolean turnLeft, boolean turnRight){
+    private TurboCar turboCar;
+
+    public Saab95(int doors, int power, int speed, Color color, String name, int x, int y, boolean turnLeft, boolean turnRight) {
         super(doors, power, speed, color, name, x, y, turnLeft, turnRight);
-    }
-
-    private void setTurboOn() {
-        turboOn = true;
-    }
-
-    private void setTurboOff() {
-        turboOn = false;
+        this.turboCar = new TurboCar();
     }
 
     public double speedFactor() {
-        double turbo = 1;
-        if (turboOn)
-            turbo = 1.3;
-        return getEnginePower() * 0.01 * turbo;
+        return getEnginePower() * 0.01 * turboCar.getTurboValue();
     }
 
+    public void setTurboOn() {
+        turboCar.setTurboOn();
+    }
+
+    public void setTurboOff() {
+        turboCar.setTurboOff();
+    }
+
+    public boolean getTurboOn() {
+        return turboCar.getTurboOn();
+    }
 }
