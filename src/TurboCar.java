4ac1bbabac7bc974053ba22772abbfb946861b6f
Kimshii <117647269+Kimshii@users.noreply.github.com>
2022-12-04 17:49:01 +0100

    Create TurboCar.java

---
+++ src/TurboCar.java
@@ -0,0 +1,30 @@
+package src;
+
+public class TurboCar {
+    private boolean turboOn;
+    private double turbo;
+
+    public TurboCar() {
+        this.turboOn = false;
+        this.turbo = 1.0;
+    }
+
+    public void setTurboOn() {
+        turboOn = true;
+    }
+
+    public void setTurboOff() {
+        turboOn = false;
+    }
+
+    public boolean getTurboOn() {
+        return this.turboOn;
+    }
+
+    public double getTurboValue() {
+        this.turbo = 1;
+        if (turboOn)
+            this.turbo = 1.3;
+        return this.turbo;
+    }
+}
