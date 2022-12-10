1143918ad36c3e1de69d5792b92ad305b75e45da
p0ntu2 <117637978+p0ntu2@users.noreply.github.com>
2022-12-10 20:24:02 +0100

    m

--- src/view/CarView.java
+++ src/view/CarView.java
@@ -24,7 +24,6 @@ public class CarView extends JFrame{
     DrawPanel drawPanel;
 
     // The controller member
-    CarController carC;
 
 
 
@@ -47,92 +46,22 @@ public class CarView extends JFrame{
 
     // Constructor
     public CarView(String framename,int x, int y ){ //CarController cc
-        this.carC = new CarController();
+        //this.carC = new CarController();
         X=x;
         Y=y;
         drawPanel = new DrawPanel(X, Y-240);
         initComponents(framename);
         }
-
-
-    public void movevolvo(int x, int y){
-        drawPanel.movevolvo(x, y);
-    }
-    public void movesaab(int x, int y){
-        drawPanel.movesaab(x, y);
-    }
-    public void movescania(int x, int y){
-        drawPanel.movescania(x, y);
-    }
-
-
-    // Sets everything in place and fits everything
-    // TODO: Take a good look and make sure you understand how these methods and components work
-    private void initComponents(String title) {
-
+        private void initComponents(String title){
         this.setTitle(title);
         this.setPreferredSize(new Dimension(X,Y));
         this.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
 
         this.add(drawPanel);
 
-
-
-        SpinnerModel spinnerModel =
-                new SpinnerNumberModel(0, //initial value
-                        0, //min
-                        100, //max
-                        1);//step
-        gasSpinner = new JSpinner(spinnerModel);
-        gasSpinner.addChangeListener(new ChangeListener() {
-            public void stateChanged(ChangeEvent e) {
-                gasAmount = (int) ((JSpinner)e.getSource()).getValue();
-            }
-        });
-
-        gasPanel.setLayout(new BorderLayout());
-        gasPanel.add(gasLabel, BorderLayout.PAGE_START);
-        gasPanel.add(gasSpinner, BorderLayout.PAGE_END);
-
-        this.add(gasPanel);
-
-        controlPanel.setLayout(new GridLayout(2,4));
-
-        controlPanel.add(gasButton, 0);
-        controlPanel.add(turboOnButton, 1);
-        controlPanel.add(liftBedButton, 2);
-        controlPanel.add(brakeButton, 3);
-        controlPanel.add(turboOffButton, 4);
-        controlPanel.add(lowerBedButton, 5);
-        controlPanel.setPreferredSize(new Dimension((X/2)+4, 200));
-        this.add(controlPanel);
-        controlPanel.setBackground(Color.CYAN);
-
-
-        startButton.setBackground(Color.blue);
-        startButton.setForeground(Color.green);
-        startButton.setPreferredSize(new Dimension(X/5-15,200));
-        this.add(startButton);
-
-
-        stopButton.setBackground(Color.red);
-        stopButton.setForeground(Color.black);
-        stopButton.setPreferredSize(new Dimension(X/5-15,200));
-        this.add(stopButton);
-
-        // This actionListener is for the gas button only
-        // TODO: Create more for each component as necessary
-        gasButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-               carC.gas(gasAmount);
-            }
-        });
-
-        
-            
-        
-    
+    // Sets everything in place and fits everything
+    // TODO: Take a good look and make sure you understand how these methods and components work
+   
         // Make the frame pack all it's components by respecting the sizes if possible.
         this.pack();
 
